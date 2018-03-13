7797f47727fbb8f27243514b67d5646b5707fec3
uwspgrps <36901959+uwspgrps@users.noreply.github.com>
2018-03-13 10:10:03 -0500

    Changed folder name for turn in
    command line didnt work

---
+++ sprint1final/const.php
@@ -0,0 +1,12 @@
+<?php
+
+define("MUSER","manchesk_l_user");
+define("MPASS","qiz13hoz");
+define("MSERVER","cnmtsrv1.uwsp.edu");
+define("MDB","manchesk_l");
+
+if (basename($_SERVER['PHP_SELF']) == "const.php") {
+  die(header("HTTP/1.0 404 Not Found"));
+}
+
+?>
